f1ccffc7d9156c2cb822f582d30addb3fd0c09d1
blankmaru <coopskywavy@gmail.com>
2021-05-10 13:40:17 +0300

    Deploy 5

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,3 +11,18 @@ export const findAll = async (req: Request, res: Response): Promise<Response | u
         return res.status(400).send({ error: err });
     }
 }
+
+export const createNewUser = async (req: Request, res: Response): Promise<Response | undefined> => {
+    try {
+        const newUser = new User({
+            Username: 'Test',
+            UserID: '12345-6789'
+        })
+
+        await newUser.save((err, doc) => {
+            return res.status(200).send({ success: true })
+        });
+    } catch(err) {
+        return res.status(400).send({ error: err });
+    }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,9 +3,11 @@ import * as express from 'express';
 const router = express.Router();
 
 import {
-    findAll
+    findAll,
+    createNewUser
 } from '../controllers/user.controller';
 
 router.get('/', findAll);
+router.post('/', createNewUser);
 
 export default router;
