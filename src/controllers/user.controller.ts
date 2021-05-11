766f6dee9a0ae11c2377fe3c57ae5621de55d427
blankmaru <coopskywavy@gmail.com>
2021-05-12 00:11:54 +0300

    Release update user route [4]

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -25,18 +25,3 @@ export const update = async (req: Request, res: Response): Promise<Response | un
         return res.status(400).send({ error: err });
     }
 }
-
-export const createNewUser = async (req: Request, res: Response): Promise<Response | undefined> => {
-    try {
-        const newUser = new User({
-            Username: 'Test',
-            UserID: '12345-6789'
-        })
-
-        await newUser.save((err, doc) => {
-            return res.status(200).send({ success: true })
-        });
-    } catch(err) {
-        return res.status(400).send({ error: err });
-    }
-}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,13 +4,10 @@ const router = express.Router();
 
 import {
     findAll,
-    createNewUser,
     update
 } from '../controllers/user.controller';
 
 router.get('/', findAll);
-router.post('/', createNewUser);
-
 router.post('/', update);
 
 export default router;
